88acd2eebeee656a369080ec333ee32c37ee9d55
Abdirahman[C Ismail <Afismail.25@gmail.com>
2021-08-05 12:25:55 -0400

    Register page styled

--- vue/src/views/Register.vue
+++ vue/src/views/Register.vue
@@ -1,6 +1,31 @@
 <template>
   <div id="register" class="text-center">
-    <form class="form-register" @submit.prevent="register">
+    <form @submit.prevent="register">
+        <div class="container">
+          <h1>Register</h1>
+          <p>Please fill in this form to create an account.</p>
+          <p role="alert" v-if="registrationErrors">{{ registrationErrorMsg }}</p>
+          <hr>
+
+          <label for="username"><b>Username</b></label>
+          <input type="text" id="username" placeholder="Enter Username" v-model="user.username" required autofocus>
+
+          <label for="psw"><b>Password</b></label>
+          <input type="password" placeholder="Enter Password" id="password" v-model="user.password" required>
+
+          <label for="psw-repeat"><b>Confirm Password</b></label>
+          <input type="password" placeholder="Confirm Password" id="confirmPassword" v-model="user.confirmPassword" required>
+          <hr>
+          <p>By creating an account you agree to our <a href="#">Terms & Privacy</a>.</p>
+
+          <button type="submit" class="registerbtn">Register</button>
+        </div>
+        
+        <div class="container signin">
+          <p>Already have an account? <router-link :to="{ name: 'login' }">Sign in</router-link>.</p>
+        </div>
+      </form>
+    <!-- <form class="form-register" @submit.prevent="register">
       <h1 class="h3 mb-3 font-weight-normal">Create Account</h1>
       <div class="alert alert-danger" role="alert" v-if="registrationErrors">
         {{ registrationErrorMsg }}
@@ -36,7 +61,7 @@
       <button class="btn btn-lg btn-primary btn-block" type="submit">
         Create Account
       </button>
-    </form>
+    </form> -->
   </div>
 </template>
 
@@ -90,4 +115,67 @@ export default {
 };
 </script>
 
-<style></style>
+<style scoped>
+div {
+  font-family: Arial, Helvetica, sans-serif;
+  background-color: black;
+}
+
+* {
+  box-sizing: border-box;
+}
+
+/* Add padding to containers */
+.container {
+  padding: 16px;
+  background-color: white;
+}
+
+/* Full-width input fields */
+input[type=text], input[type=password] {
+  width: 100%;
+  padding: 15px;
+  margin: 5px 0 22px 0;
+  display: inline-block;
+  border: none;
+  background: #f1f1f1;
+}
+
+input[type=text]:focus, input[type=password]:focus {
+  background-color: #ddd;
+  outline: none;
+}
+
+/* Overwrite default styles of hr */
+hr {
+  border: 1px solid #f1f1f1;
+  margin-bottom: 25px;
+}
+
+/* Set a style for the submit button */
+.registerbtn {
+  background-color: #04AA6D;
+  color: white;
+  padding: 16px 20px;
+  margin: 8px 0;
+  border: none;
+  cursor: pointer;
+  width: 100%;
+  opacity: 0.9;
+}
+
+.registerbtn:hover {
+  opacity: 1;
+}
+
+/* Add a blue text color to links */
+a {
+  color: dodgerblue;
+}
+
+/* Set a grey background color and center the text of the "sign in" section */
+.signin {
+  background-color: #f1f1f1;
+  text-align: center;
+}
+</style>
