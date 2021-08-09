9eef8b17a801ad31fc684a93012730bcea03a268
Katy Richardson <katy.berkman@gmail.com>
2021-08-09 19:01:25 -0400

    Backend Goals

--- java/src/main/java/com/techelevator/controller/GoalController.java
+++ java/src/main/java/com/techelevator/controller/GoalController.java
@@ -3,9 +3,14 @@ package com.techelevator.controller;
 import com.techelevator.dao.EventDAO;
 import com.techelevator.dao.GoalDAO;
 import com.techelevator.dao.UserDAO;
+import com.techelevator.model.Event;
+import com.techelevator.model.Goal;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.security.Principal;
+import java.util.List;
 
 @PreAuthorize("isAuthenticated()")
 @RestController
@@ -20,5 +25,26 @@ public class GoalController {
     public GoalController(GoalDAO goalDAO, UserDAO userDAO) {
         this.goalDAO = goalDAO;
         this.userDAO = userDAO;
+    }
+
+    @RequestMapping(path = "/goals/premade", method = RequestMethod.GET)
+    public List<Goal> getPremadeGoals() {
+        return goalDAO.retrievePremadeGoals();
+    }
+
+    @RequestMapping(path = "/goals/{goalId}", method = RequestMethod.GET)
+    public Goal getGoalByGoalId(@PathVariable int goalId) {
+        return goalDAO.retrieveGoalById(goalId);
+    }
+
+    @RequestMapping(path = "/dashboard/goals", method = RequestMethod.GET)
+    public List<Goal> getGoalsByUserId(Principal principal) {
+        return goalDAO.retrieveGoalsByUser(userDAO.findIdByUsername(principal.getName()));
+    }
+
+    @ResponseStatus(HttpStatus.CREATED)
+    @RequestMapping(path = "/createGoal", method = RequestMethod.POST)
+    public void createNewGoal(@RequestBody Goal goal) {
+        goalDAO.addNewGoal(goal);
     }
 }
--- java/src/main/java/com/techelevator/dao/JdbcGoalDAO.java
+++ java/src/main/java/com/techelevator/dao/JdbcGoalDAO.java
@@ -30,32 +30,134 @@ public class JdbcGoalDAO implements GoalDAO {
         SqlRowSet results = jdbcTemplate.queryForRowSet(sql);
 
             while (results.next()) {
-            Goal goal = mapRowToGoal(results);
+            Goal goal = mapRowToPremadeGoal(results);
             premadeGoalList.add(goal);
         }
         return premadeGoalList;
     }
 
-
-
-
     @Override
     public Goal retrieveGoalById(int goalId) {
-        return null;
+        Goal goal = null;
+
+        String sql = "SELECT goal_id, goal_name, start_date, end_date, user_activity_goal, " +
+                "goal_units, user_id FROM goals WHERE goal_id = ?";
+
+        SqlRowSet result = jdbcTemplate.queryForRowSet(sql, goalId);
+
+        if (result.next()) {
+            goal = mapRowToGoal(result);
+        }
+        return goal;
     }
 
     @Override
     public List<Goal> retrieveGoalsByUser(int userId) {
-        return null;
+        List<Goal> goalList = new ArrayList<Goal>();
+
+        String sql = "SELECT goal_id, goal_name, start_date, end_date, user_activity_goal, " +
+                "goal_units, user_id FROM goals WHERE user_id = ?";
+
+        SqlRowSet results = jdbcTemplate.queryForRowSet(sql, userId);
+
+        while (results.next()) {
+            Goal goal = mapRowToGoal(results);
+            goalList.add(goal);
+        }
+
+        return goalList;
     }
 
     @Override
     public Goal addNewGoal(Goal goal) {
-        return null;
+        String sql = "INSERT INTO goals (goal_id, goal_name, start_date, end_date, user_activity_goal, goal_units, " +
+        "user_id) VALUES (DEFAULT, ?, ?, ?, ?, ?, ?) RETURNING goal_id";
+
+        SqlRowSet results = jdbcTemplate.queryForRowSet(sql, goal.getGoalName(), goal.getStartDate(), goal.getEndDate(),
+                goal.getActivityGoal(), goal.getActivityUnits(), goal.getUserId());
+
+        int newGoalId = 0;
+        if (results.next()) {
+            newGoalId = results.getInt("goal_id");
+        }
+        goal.setGoalId(newGoalId);
+        return goal;
     }
 
-    private Goal mapRowToGoal(SqlRowSet results){
+    private Goal mapRowToPremadeGoal(SqlRowSet results){
         Goal goal = new Goal();
+
+        goal.setGoalId(results.getInt("premade_goal_id"));
+        goal.setGoalName(results.getString("premade_goal_name"));
+        goal.setDescription(results.getString("premade_goal_description"));
+        goal.setActivityGoal(results.getDouble("premade_user_activity_goal"));
+        goal.setActivityUnits(results.getString("premade_goal_units"));
+
+        List <String> activityListForGoal = getActivityListForPremadeGoal(results.getInt("premade_goal_id"));
+        if(activityListForGoal != null) {
+            goal.setActivityType(activityListForGoal);
+        }
+
         return goal;
+    }
+
+    private List<String> getActivityListForPremadeGoal(int premadeGoalId) {
+
+        String sql = "SELECT activity_type.activity_type FROM activity_type " +
+                "JOIN premade_goal_activity_type ON premade_goal_activity_type.activity_type_id = activity_type.activity_type_id " +
+                "JOIN premade_goals ON premade_goals.premade_goal_id = premade_goal_activity_type.premade_goal_id " +
+                "WHERE premade_goals.premade_goal_id = ?";
+
+        SqlRowSet results = jdbcTemplate.queryForRowSet(sql, premadeGoalId);
+
+        List<String> activityListForGoal = new ArrayList<>();
+
+        while (results.next()) {
+
+            String activity_type = results.getString("activity_type");
+            activityListForGoal.add(activity_type);
+        }
+
+        return activityListForGoal;
+
+    }
+    private Goal mapRowToGoal(SqlRowSet result){
+        Goal goal = new Goal();
+
+        goal.setGoalId(result.getInt("goal_id"));
+        goal.setGoalName(result.getString("goal_name"));
+        goal.setStartDate(result.getDate("start_date"));
+        goal.setEndDate(result.getDate("end_date"));
+        goal.setActivityGoal(result.getDouble("user_activity_goal"));
+        goal.setActivityUnits(result.getString("goal_units"));
+        goal.setUserId(result.getInt("user_id"));
+
+        List <String> activityListForGoal = getActivityListForGoal(result.getInt("goal_id"));
+        if(activityListForGoal != null) {
+            goal.setActivityType(activityListForGoal);
+        }
+
+        return goal;
+    }
+
+    private List<String> getActivityListForGoal(int goalId) {
+
+        String sql = "SELECT activity_type.activity_type FROM activity_type " +
+                "JOIN goal_activity_type ON goal_activity_type.activity_type_id = activity_type.activity_type_id " +
+                "JOIN goals ON goals.goal_id = goal_activity_type.goal_id " +
+                "WHERE goals.goal_id = ?";
+
+        SqlRowSet results = jdbcTemplate.queryForRowSet(sql, goalId);
+
+        List<String> activityListForGoal = new ArrayList<>();
+
+        while (results.next()) {
+
+            String activity_type = results.getString("activity_type");
+            activityListForGoal.add(activity_type);
+        }
+
+        return activityListForGoal;
+
     }
 }
