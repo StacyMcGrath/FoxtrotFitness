b32605f5e1c77d215291a7af12c5fe8b7de41e26
Abdirahman[C Ismail <Afismail.25@gmail.com>
2021-08-09 13:57:25 -0400

    Some styling on EventForm

--- vue/src/views/NewEventFormPage.vue
+++ vue/src/views/NewEventFormPage.vue
@@ -1,15 +1,21 @@
 <template>
+<div>
   <new-event-form/>
+  <test-new-event-form/>
+</div>
 </template>
 
 <script>
 import NewEventForm from '../components/NewEventForm.vue'
+import TestNewEventForm from '../components/TestNewEventForm.vue'
+
 export default {
-  components: { NewEventForm },
+  components: { NewEventForm, TestNewEventForm },
 
 }
 </script>
 
 <style>
+    
 
 </style>
