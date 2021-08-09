f9af137b611828f6cc1ee07057529700aa7faf7d
Abdirahman[C Ismail <Afismail.25@gmail.com>
2021-08-09 14:14:09 -0400

    newe event form page form

--- vue/src/views/NewEventFormPage.vue
+++ vue/src/views/NewEventFormPage.vue
@@ -1,16 +1,16 @@
 <template>
-<div>
+
   <new-event-form/>
-  <test-new-event-form/>
-</div>
+  
+
 </template>
 
 <script>
 import NewEventForm from '../components/NewEventForm.vue'
-import TestNewEventForm from '../components/TestNewEventForm.vue'
+
 
 export default {
-  components: { NewEventForm, TestNewEventForm },
+  components: { NewEventForm },
 
 }
 </script>
