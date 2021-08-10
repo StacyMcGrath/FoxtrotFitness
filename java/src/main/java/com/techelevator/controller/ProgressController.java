b9ac1f3075b13a3261d11f7d95b856014d6cf05e
Katy Richardson <katy.berkman@gmail.com>
2021-08-10 15:54:40 -0400

    progress controller update

--- java/src/main/java/com/techelevator/controller/ProgressController.java
+++ java/src/main/java/com/techelevator/controller/ProgressController.java
@@ -1,13 +1,16 @@
 package com.techelevator.controller;
 
+import com.techelevator.dao.GoalDAO;
 import com.techelevator.dao.ProgressDAO;
 import com.techelevator.dao.UserDAO;
 import com.techelevator.model.Event;
+import com.techelevator.model.Goal;
 import com.techelevator.model.Progress;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.security.Principal;
+import java.util.ArrayList;
 import java.util.List;
 
 @PreAuthorize("isAuthenticated()")
@@ -17,10 +20,12 @@ public class ProgressController {
 
     private ProgressDAO progressDAO;
     private UserDAO userDAO;
+    private GoalDAO goalDAO;
 
-    public ProgressController(ProgressDAO progressDAO, UserDAO userDAO) {
+    public ProgressController(ProgressDAO progressDAO, UserDAO userDAO, GoalDAO goalDAO) {
         this.progressDAO = progressDAO;
         this.userDAO = userDAO;
+        this.goalDAO = goalDAO;
     }
 
     @RequestMapping(path = "/progress/{eventId}", method = RequestMethod.GET)
@@ -33,9 +38,15 @@ public class ProgressController {
         return progressDAO.retrieveProgressByEventForUser(eventId, userDAO.findIdByUsername(principal.getName()));
     }
 
-    @RequestMapping(path = "/progress/goal/{goalId}", method = RequestMethod.GET)
-    public List<Progress> getProgressByGoalForIndividualUser(@PathVariable int goalId, Principal principal) {
-        return progressDAO.retrieveProgressByGoalForUser(goalId, userDAO.findIdByUsername(principal.getName()));
-    }
+//    @RequestMapping(path = "/progress/goal", method = RequestMethod.GET)
+//    public List<Progress> getProgressByGoalForIndividualUser(Principal principal) {
+//        List<Goal> goals = goalDAO.retrieveGoalsByUser(userDAO.findIdByUsername(principal.getName()));
+//        List<Progress> goalsProgress = new ArrayList<>();
+//        for(Goal goal: goals) {
+//            List <Progress> individualProgressList = progressDAO.retrieveProgressByGoalForUser(goal.getGoalId(), goal.getUserId());
+//
+//        }
+//        return ;
+//    }
 
 }
